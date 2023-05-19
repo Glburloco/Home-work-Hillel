354c0568abaedcb00e74c5e2c5ff87ed15f5bc86
user@WIN-UT9R537AKQQ <generateptgpyloc@gmail.com>
2023-05-19 22:50:52 +0300

    virus brainwashed fix

--- lesson_13_virus.py
+++ lesson_13_virus.py
@@ -2,15 +2,15 @@ n = int(input("Enter number of files N: "))
 files = {}
 
 for _ in range(n):
-    file, extension = input("Enter file name and operation (X, R, W): ").split(maxsplit=1)
-    files[file] = [extension.lower(), extension.upper()]
+    file, extension = input("Enter file name and operation (X, R, W): ").split()
+    files[file] = extension.upper()
 
 m = int(input("Enter the number of file requests M: "))
 requests = []
 
 for _ in range(m):
     action, file = input("Enter the query (option file name): ").split()
-    requests.append((action, file))
+    requests.append((action.lower(), file))
 
 actions = {
     'write': 'W',
@@ -20,11 +20,7 @@ actions = {
 print("Output:")
 for action, file in requests:
     file_extension = files.get(file, '')
-    if action == 'write' and 'W' in file_extension:
-        print("OK")
-    elif action == 'read' and 'R' in file_extension:
-        print("OK")
-    elif action == 'execute' and 'X' in file_extension:
+    if actions.get(action) == file_extension:
         print("OK")
     else:
         print("Access denied")

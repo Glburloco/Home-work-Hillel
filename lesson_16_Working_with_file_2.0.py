e002fe478130dabaf475bf1f968a31abc8824c4c
user@WIN-UT9R537AKQQ <generateptgpyloc@gmail.com>
2023-05-24 22:13:13 +0300

    result save plus def

--- lesson_16_Working_with_file_2.0.py
+++ lesson_16_Working_with_file_2.0.py
@@ -1,39 +1,24 @@
-# Task_1
-import pickle
-
-list_dicts = [
-    {'key': 'val1'},
-    {'key': 'val2'},
-    {'key': 'val3'}
-]
-with open('data.pickle.txt', 'wb') as file:
-    pickle.dump(list_dicts, file)
-
-
-with open('data.pickle.txt', 'rb') as file:
-    list_dicts = pickle.load(file)
-
-for dictionary in list_dicts:
-    print(dictionary)
-
-# Task_2
 import json
+def mge_dicts(a, b):
+    c = a.copy()
+    for key, value in b.items():
+        if key in c:
+            if isinstance(c[key], list):
+                c[key].append(value)
+            else:
+                c[key] = [c[key], value]
+        else:
+            c[key] = value
+    return c
 
 A = {'key': 1, 'key2': True}
 B = {'key': 'Hello'}
 
-C = {}
-for key, value in A.items():
-    if key in B:
-        C[key] = [value, B[key]]
-    else:
-        C[key] = value
-
-for key, value in B.items():
-    if key not in C:
-        C[key] = value
+C = mge_dicts(A, B)
 
 with open('result.json', 'w') as file:
     json.dump(C, file)
 
-
+print("Значення словника A:", A)
+print("Значення словника B:", B)
+print("Значення словника C:", C)

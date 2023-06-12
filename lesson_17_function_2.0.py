c2a115eb31e7d4592b9583ec904e3272a910d0ae
user@WIN-UT9R537AKQQ <generateptgpyloc@gmail.com>
2023-06-12 19:34:54 +0300

    function 2.0 rename

--- lesson_17_function_2.0.py
+++ lesson_17_function_2.0.py
@@ -1,11 +1,14 @@
-#Task_1
+# Task_1
 def change(lst):
     lst[0], lst[-1] = lst[-1], lst[0]
-#Task_2
+
+
+# Task_2
 def to_dict(lst):
     return {item: item for item in lst}
 
-#Task_3
+
+# Task_3
 def sum_range(start, end):
     if start > end:
         start, end = end, start
